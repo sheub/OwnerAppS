6a8a69c27e78013a38b7c66cd250739c1df61612
Manly971 <manlygwada_971@hotmail.fr>
2018-05-12 13:37:22 +0200

    Add WhiteOctoberPagerfantaBundle for paginate

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true]
 ];
